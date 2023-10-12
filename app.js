0d4279bc6066129f1564b9dd9916639643f2b3b6
JayM647 <jemarmrrs@gmail.com>
2023-10-11 22:55:26 -0400

    working on styling, added spacing and particles

---
+++ app.js
@@ -0,0 +1,30 @@
+particlesJS('particles-js',
+    {
+        "particles": {
+            "number": {
+                "value": 30,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "polygon"
+            },
+            /* ... more configuration ... */
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                /* ... more configuration ... */
+            }
+        }
+    }
+);
+
